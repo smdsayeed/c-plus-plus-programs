fec3e04c183e91434ca5805d9b9ce611bb682b1e
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-16 13:50:29 +0530

    Add files via upload

---
+++ first_progam.cpp
@@ -0,0 +1,7 @@
+#include<iostream> //header file
+//empty lines are ignored.
+using namespace std; //. The identifiers of the C++ standard library are defined in a namespace called std
+    int main(){
+       cout<<"hello world";
+        return 0;
+    }
