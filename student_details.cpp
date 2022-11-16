26c602179959bc648d7ccf2f120a9db23c6a89dc
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-16 14:09:27 +0530

    Add files via upload

---
+++ student_details.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+    int main(){
+        char name[20],course[10];
+        int rno; int fee=25000;
+        cout<<"Enter your name\n";
+        cin>>name;
+        cout<<"Enter your roll number\n";
+        cin>>rno;
+        cout<<"Enter your course\n";
+        cin>>course;
+
+        cout<<"Name     :"<<name<<"\n";
+        cout<<"course   :"<<course<<"\n";
+        cout<<"roll-no  :"<<rno<<"\n";
+        cout<<"Fee      :"<<fee<<"\n";
+
+
+        
+    }
