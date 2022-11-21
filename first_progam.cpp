1b4f7ea06bdca6d2892d2822cdc7bff1d9de0f7a
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-21 16:37:00 +0530

    Update first_progam.cpp

--- first_progam.cpp
+++ first_progam.cpp
@@ -1,7 +1,10 @@
-#include<iostream> //header file
+#include<iostream>    //header file,here stream the data flow
+                                                                
 //empty lines are ignored.
-using namespace std; //. The identifiers of the C++ standard library are defined in a namespace called std
+
+using namespace std;  //namespace are used to organize code into logical groups
+// and to prevent name collisions that can occur especilaly when your code ase includes multiple library.
     int main(){
-       cout<<"hello world";
+       cout<<"hello world"; //  "<<" This is a insertion operator,">>" This is a extraction operator
         return 0;
     }
